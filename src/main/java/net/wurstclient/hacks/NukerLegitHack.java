72d21d2ba598e0816e66ba1b7699e6cd8024ac0c
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2022-03-01 19:33:06 +0800

    Update NukerLegitHack.java

--- src/main/java/net/wurstclient/hacks/NukerLegitHack.java
+++ src/main/java/net/wurstclient/hacks/NukerLegitHack.java
@@ -106,7 +106,7 @@ public final class NukerLegitHack extends Hack
 		EVENTS.remove(RenderListener.class, this);
 		
 		// resets
-		MC.options.keyAttack.setPressed(false);
+		MC.options.attackKey.setPressed(false);
 		currentBlock = null;
 		if(!lockId.isChecked())
 			id.setBlock(Blocks.AIR);
@@ -164,7 +164,7 @@ public final class NukerLegitHack extends Hack
 		
 		// reset if no block was found
 		if(currentBlock == null)
-			MC.options.keyAttack.setPressed(false);
+			MC.options.attackKey.setPressed(false);
 	}
 	
 	private ArrayList<BlockPos> getValidBlocks(double range,
@@ -221,15 +221,15 @@ public final class NukerLegitHack extends Hack
 				
 			// if attack key is down but nothing happens, release it for one
 			// tick
-			if(MC.options.keyAttack.isPressed()
+			if(MC.options.attackKey.isPressed()
 				&& !MC.interactionManager.isBreakingBlock())
 			{
-				MC.options.keyAttack.setPressed(false);
+				MC.options.attackKey.setPressed(false);
 				return true;
 			}
 			
 			// damage block
-			MC.options.keyAttack.setPressed(true);
+			MC.options.attackKey.setPressed(true);
 			
 			return true;
 		}
