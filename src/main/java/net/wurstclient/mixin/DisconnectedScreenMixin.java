c40e6ec9d79ea2cceab47bc1e34da5f2c2f14891
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2022-04-22 14:41:53 +0800

    1

--- src/main/java/net/wurstclient/mixin/DisconnectedScreenMixin.java
+++ src/main/java/net/wurstclient/mixin/DisconnectedScreenMixin.java
@@ -52,12 +52,12 @@ public class DisconnectedScreenMixin extends Screen
 			Math.min(height / 2 + reasonHeight / 2 + 9, height - 30);
 		
 		addDrawableChild(new ButtonWidget(backButtonX, backButtonY + 24, 200,
-			20, Text.literal("Reconnect"),
+			20, Text.literal("自动重连"),
 			b -> LastServerRememberer.reconnect(parent)));
 		
 		autoReconnectButton = addDrawableChild(
 			new ButtonWidget(backButtonX, backButtonY + 48, 200, 20,
-				Text.literal("AutoReconnect"), b -> pressAutoReconnect()));
+				Text.literal("自动重新连接"), b -> pressAutoReconnect()));
 		
 		if(WurstClient.INSTANCE.getHax().autoReconnectHack.isEnabled())
 			autoReconnectTimer = 100;
@@ -82,11 +82,11 @@ public class DisconnectedScreenMixin extends Screen
 		
 		if(!autoReconnect.isEnabled())
 		{
-			autoReconnectButton.setMessage(Text.literal("AutoReconnect"));
+			autoReconnectButton.setMessage(Text.literal("自动重连"));
 			return;
 		}
 		
-		autoReconnectButton.setMessage(Text.literal("AutoReconnect ("
+		autoReconnectButton.setMessage(Text.literal("自动重连 ("
 			+ (int)Math.ceil(autoReconnectTimer / 20.0) + ")"));
 		
 		if(autoReconnectTimer > 0)
