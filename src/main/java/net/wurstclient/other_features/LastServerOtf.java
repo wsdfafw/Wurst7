d5549f0237354854643c729eaa8945003cbd23d8
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2022-12-15 12:17:50 +0800

    s

--- src/main/java/net/wurstclient/other_features/LastServerOtf.java
+++ src/main/java/net/wurstclient/other_features/LastServerOtf.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * Copyright (c) 2014-2022 Wurst-Imperium and contributors.
  *
@@ -18,30 +17,7 @@ public final class LastServerOtf extends OtherFeature
 {
 	public LastServerOtf()
 	{
-		super("上一个服务器","Wurst在服务器选择屏幕上添加了一个“上一个服务器”按钮,该按钮会自动将您带回上次玩的服务器.\n\n当你被踢和/或有很多服务器时很有用.");
+		super("上一个服务器",
+			"Wurst在服务器选择屏幕上添加了一个“上一个服务器”按钮,该按钮会自动将您带回上次玩的服务器.\n\n当你被踢和/或有很多服务器时很有用"
 	}
 }
-=======
-/*
- * Copyright (c) 2014-2022 Wurst-Imperium and contributors.
- *
- * This source code is subject to the terms of the GNU General Public
- * License, version 3. If a copy of the GPL was not distributed with this
- * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
- */
-package net.wurstclient.other_features;
-
-import net.wurstclient.DontBlock;
-import net.wurstclient.SearchTags;
-import net.wurstclient.other_feature.OtherFeature;
-
-@SearchTags({"last server"})
-@DontBlock
-public final class LastServerOtf extends OtherFeature
-{
-	public LastServerOtf()
-	{
-		super("上一个服务器","Wurst在服务器选择屏幕上添加了一个“上一个服务器”按钮,该按钮会自动将您带回上次玩的服务器.\n\n当你被踢和/或有很多服务器时很有用.");
-	}
-}
->>>>>>> upstream/1.19
